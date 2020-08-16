cdac4d04f1804eaa5139529d379ac08807ada0bb
Oktay Dinler <oktay@oktaydinler.xyz>
2020-08-16 20:29:57 +0200

    Update main.go docs

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 Run your Go Advent of Code solutions with a single command.
 Write your solution and Elver will take care of the rest.
 
-How it works
+How It Works
 
 Elver uses plugin build mode to generate a `.so` file to dynamically look up
 the solutions.
@@ -19,16 +19,22 @@ A solver must be exported and it's name satisfy the following regex:
 
 	(Day)([1-9]|1[0-9]|2[0-5])(A|B)
 
-Code example
-
-	package main
-
-	import "errors"
+E.g.:
 
 	func Day1A(input string) (interface{}, error) {
 	    return 42, nil
 	}
 
+Solvers are workspaced by the Advent of Code year which is also used as the
+folder name.
+
+Code Example
+
+	package main
+	import "errors"
+	func Day1A(input string) (interface{}, error) {
+	    return 42, nil
+	}
 	func Day1B(input string) (interface{}, error) {
 	    return nil, errors.New("Not implemented")
 	}
